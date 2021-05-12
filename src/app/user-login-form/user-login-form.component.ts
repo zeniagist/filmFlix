664cc4ef0241d80ef32a39f19c41ac60c480623b
Zenia Gist <zeniagist@gmail.com>
2021-05-12 12:20:17 -0500

    setup functionality for  user login form

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { UserLoginService } from '../fetch-api-data.service';
+
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-user-login-form',
@@ -7,9 +14,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserLoginFormComponent implements OnInit {
 
-  constructor() { }
+  @Input() userData ={ Username: '', Password: ''};
 
-  ngOnInit(): void {
+  constructor(
+    public fetchApiData: UserLoginService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar,
+    public router: Router
+  ) { }
+
+  ngOnInit(): void { }
+
+  /**
+   * function to login user
+   */
+  loginUser(): void {
+    this.fetchApiData.userLogin(this.userData).subscribe((response) => {
+      this.dialogRef.close();
+      localStorage.setItem('user', response.user.Username);
+      localStorage.setItem('token', response.token);
+      this.snackBar.open('User logged in successfully!', 'OK', {
+        duration: 2000
+      });
+      this.router.navigate(['movies']);
+    }, (response) => {
+      console.log(response);
+      this.snackBar.open(response, 'OK', {
+        duration: 2000
+      });
+    });
   }
-
 }
