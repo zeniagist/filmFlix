adf528e607261a78eb6439745ef63a3e56a61108
Zenia Gist <zeniagist@gmail.com>
2021-05-12 12:06:42 -0500

    added user login form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
@@ -11,11 +12,17 @@ export class AppComponent {
   title = 'myFlix-Angular-client';
 
   constructor(public dialog: MatDialog) { }
-// This is the function that will open the dialog when the signup button is clicked  
-openUserRegistrationDialog(): void {
+
+  // Directs to registration modal
+  openUserRegistrationDialog(): void {
     this.dialog.open(UserRegistrationFormComponent, {
-// Assigning the dialog a width
+  // Assigning the dialog a width
     width: '280px'
     });
+  }
+
+  // Directs to login modal
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {});
   }
 }
