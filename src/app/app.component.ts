daf907ae17dc0b375adbb4162609f2f2384ac1b7
Zenia Gist <zeniagist@gmail.com>
2021-05-18 09:26:50 -0500

    setup movie-card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
@@ -24,5 +25,12 @@ export class AppComponent {
   // Directs to login modal
   openUserLoginDialog(): void {
     this.dialog.open(UserLoginFormComponent, {});
+  }
+
+  // Directs to movie-card
+  openMoviesDialog(): void {
+    this.dialog.open(MovieCardComponent, {
+      width: '500px'
+    });
   }
 }
